466985b044f3566dc955c39cd0ca6c39c66f56e3
Mahtab <mahtabhoushmand@yahoo.de>
2022-08-15 11:52:03 +0200

    add comments to rainforest api

--- RainforestAPI/main.py
+++ RainforestAPI/main.py
@@ -1,8 +1,9 @@
-import json
+# import necessary libraries
 import requests
 import requests_cache
 import pandas as pd
 
+# set up request cache to reduce identical API calls
 requests_cache.install_cache('rainforest_cache')
 
 # Set up the request parameters
@@ -10,15 +11,14 @@ params = {
 'api_key': '1DD6DF4727184B8A9214AD1872A248FB',
   'type': 'deals',
   'amazon_domain': 'amazon.com',
-  'category_id': '1266092011',
+  'category_id': '1266092011',  # id for category TV & Video
   'max_page': 5
 }
 
-# make the http GET request to Rainforest API
+# send the http GET request to Rainforest API
 api_result = requests.get('https://api.rainforestapi.com/request', params)
 
-# Deals_results als python object
+# convert deals results to csv using pandas
 deals_results = api_result.json()["deals_results"]
 df = pd.DataFrame(deals_results)
-pd.set_option('display.max_columns', None)  # or 1000
 df.to_csv("rainforest_tvdeals_20220705.csv")
