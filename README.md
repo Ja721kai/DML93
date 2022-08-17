8f3fa0e923f1b9ded6569e41fe1dc9d2c1056e6a
Jan-Xanevo <jan@xanevo.com>
2022-08-17 17:22:55 +0200

    Add comments for py scripts and README sections for notebooks

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # DML93
+
+## Python Notebooks
+This section covers a short description of Python notebooks that are stored in `~\notebooks\`.
+### merge_data.ipynb
+Output data (in CSV format) from (Rainforest API)[https://www.rainforestapi.com/] and (Target)[https://www.target.com/] (through web scraping) are being loaded and merged into a uniform data structure.
+
+The result can then again be used within a map-reduce routine to simply the dataset and gather information.
+
+A sample of the output file is located within this repository as well.
+
+### target_scraping.ipynb
+The "All Deals : TVs" section on (Target)[https://www.target.com/] is being scraped by using Selenium to simulate a real browser and imitate user behaviour.
+
+Since Target using lazy loading, javascript rendering and scrolling are required to load data about TV deals.
+
+The notebook also deals with pagination and selects the next page as long as there are more TV deals that haven't been discovered through the Selenium web browser yet.
+
+After aggregating information about all TV deals, these are being stored (one TV per row) in a CSV output file.
+
+Samples of output files are located within this repository as well.
+
--- build_dashboard_dataset/powerBI.py
+++ build_dashboard_dataset/powerBI.py
@@ -3,11 +3,13 @@ import os
 
 
 def get_items_in_partr_row_simple(line: str):
+    # Split map-reduce output file lines into their key and value.
     keyvalue = line.split("\t")
     return keyvalue[0], keyvalue[1].strip()
 
 
 def get_items_in_partr_row_ratings(line: str):
+    # Split map-reduce output file with more complex data structure into key, value1 and value2.
     name = line.split(":")[0]
     rating = line.split(":")[1].split("\t")[0]
     count = line.split("\t")[1]
@@ -15,6 +17,7 @@ def get_items_in_partr_row_ratings(line: str):
 
 
 def get_dir_paths(path, startswith_string):
+    # List all OS paths to different map-reduce output folders that follow the taxonomy 'output-<mapreduce-function>'.
     dirs = os.listdir(path)
     count_dirs = list(filter(lambda k: k.startswith(startswith_string), dirs))
     paths = [os.path.join(path, x) for x in count_dirs]
@@ -22,6 +25,8 @@ def get_dir_paths(path, startswith_string):
 
 
 def deal_count_by_filter(path_to_hadoop_results="../mapreduce/"):
+    # Collect all map-reduce outputs that count the amount of eCommerce deals and aggregate them with their respective
+    # names, counts and filter_type (e.g. brand, data source or product type).
     paths = get_dir_paths(path_to_hadoop_results, "output-count")
 
     results = {
@@ -31,7 +36,7 @@ def deal_count_by_filter(path_to_hadoop_results="../mapreduce/"):
     }
     for path in paths:
         filter_type = path.split("/")[-1].split("-")[-1]
-        with open(os.path.join(path,"part-r-00000"), "r") as f:
+        with open(os.path.join(path, "part-r-00000"), "r") as f:
             for line in f.readlines():
                 name, count = get_items_in_partr_row_simple(line)
                 results["filter_type"] += [filter_type]
@@ -42,6 +47,8 @@ def deal_count_by_filter(path_to_hadoop_results="../mapreduce/"):
 
 
 def rating_by_filter(path_to_hadoop_results="../mapreduce/"):
+    # Collect all map-reduce outputs that contain ratings of eCommerce deals and aggregate them with their respective
+    # names, ratings, rating_counts and filter_type (e.g. brand, data source or product type).
     paths = get_dir_paths(path_to_hadoop_results, "output-rating")
 
     results = {
@@ -64,6 +71,8 @@ def rating_by_filter(path_to_hadoop_results="../mapreduce/"):
 
 
 def pricediffavg(path_to_hadoop_results="../mapreduce/"):
+    # Load map-reduce outputs of computed average price differences between list prices and deal prices in order
+    # to aggregate them with their respective data source and average price difference.
     path = get_dir_paths(path_to_hadoop_results, "output-pricediffavg")[0]
 
     results = {
